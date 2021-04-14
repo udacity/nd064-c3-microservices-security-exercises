d3747071e51c1d9ff3686414beabf31bf4bd45d0
Nick <nreva@snap.com>
2021-04-14 10:05:07 -1000

    add vuln_app

--- lesson-4-kubernetes-attack-surface-and-hardening/exercises/starter/docker-clean.sh
+++ lesson-4-kubernetes-attack-surface-and-hardening/exercises/starter/docker-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker rm -f $(docker ps -qa)
+docker  rm -f$(docker ps -qa)
 docker volume rm $(docker volume ls -q)
 cleanupdirs="/var/lib/etcd* /etc/kubernete* /etc/cni* /opt/cni* /var/lib/cni* /var/run/calico* /var/lib/kubelet*"
 for dir in $cleanupdirs; do
