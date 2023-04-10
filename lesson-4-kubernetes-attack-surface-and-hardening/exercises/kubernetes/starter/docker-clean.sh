c6e79818dfa05ce7665978858518ee9c754ffd20
Nick Reva <nikita.reva@gmail.com>
2023-04-09 21:28:23 -0700

    refactor order to support deepfence

---
+++ lesson-4-kubernetes-attack-surface-and-hardening/exercises/kubernetes/starter/docker-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker  rm -f$(docker ps -qa)
+docker volume rm $(docker volume ls -q)
+cleanupdirs="/var/lib/etcd* /etc/kubernete* /etc/cni* /opt/cni* /var/lib/cni* /var/run/calico* /var/lib/kubelet*"
+for dir in $cleanupdirs; do
+  echo "Removing $dir"
+  rm -Rf $dir
+done
