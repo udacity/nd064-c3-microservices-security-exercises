b4806cec3e117d8429aba8152fe4cb02ac545ea0
Sahil Goyal <sahilgoyal@Sahils-MacBook-Pro.local>
2025-04-24 16:17:18 +0530

    updated and fixed the small issues

--- lesson-6-runtime-monitoring-and-incident-response/exercises/starter/bootstrap.sh
+++ lesson-6-runtime-monitoring-and-incident-response/exercises/starter/bootstrap.sh
@@ -15,16 +15,16 @@ echo "[TASK 3] Disable apparmor"
 systemctl stop apparmor
 systemctl disable apparmor
 
-echo "[TASK 4] Set up rke user"
-useradd rke
-usermod -a -G docker rke
-systemctl restart docker
+# echo "[TASK 4] Set up rke user"
+# useradd rke
+# usermod -a -G docker rke
+# systemctl restart docker
 
-echo "[TASK 5] Copy auth_keys for rke user"
-mkdir -p /home/rke/.ssh
-usermod -d /home/rke/ rke
-cp /root/.ssh/authorized_keys /home/rke/.ssh
-chown rke /home/rke -R
+# echo "[TASK 5] Copy auth_keys for rke user"
+# mkdir -p /home/rke/.ssh
+# usermod -d /home/rke/ rke
+# cp /root/.ssh/authorized_keys /home/rke/.ssh
+# chown rke /home/rke -R
 
 echo "[TASK 6] Install kubectl"
 # Download and install kubectl
